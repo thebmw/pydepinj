e16b412e1450a508f875fbbfb812f0a12e446239
thebmw <thebmw@thebmw.tech>
2023-09-18 06:25:43 +0000

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(name="pydepinj",
+      version="0.1.0",
+      description="A basic dependency injection framework",
+      license='MIT',
+      author="Brandon Warner",
+      author_email="bwarner@thebmw.tech",
+      url='https://github.com/thebmw/pydepinj',
+      packages=['pydepinj']
+)
